b511de09fa66133c7b2412799d5aa500253ffd4f
Keeze392 <94383342+Keeze392@users.noreply.github.com>
2026-02-03 14:45:05 +0100

    cleanup and better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,103 @@
 use std::time::Duration;
+use std::env::args;
+
 use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter, WriteType};
 use btleplug::platform::{Adapter, Manager, Peripheral};
+
 use tokio::time;
+
 use uuid::{uuid, Uuid};
-use std::env::args;
 
 #[tokio::main]
 async fn main() {
-    let power_mode = args().nth(1).expect("missing input, available mode: wake, sleep");
+    let power_mode: String = match args().nth(1) {
+        Some(v) => v,
+        None => panic!("missing input, available mode: wake, sleep")
+    };
 
     // this uuid is for power mode.
     const BASE_STATION_UUID: Uuid = uuid!("00001525-1212-efde-1523-785feabcd124");
 
-    // get ble adapter and start
-    let manager = Manager::new().await.unwrap();
-    let adapters = manager.adapters().await.unwrap();
-    let central = adapters.into_iter().nth(0).unwrap();
+    let manager: Manager = match Manager::new().await {
+        Ok(m) => m,
+        Err(e) => panic!("Creating Manager failed {}", e)
+    };
+
+    // get adapters from manager
+    let adapters: Vec<Adapter> = match manager.adapters().await {
+        Ok(a) => a,
+        Err(e) => panic!("trying get adapters failed {}", e)
+    };
+
+    // get device from adapters
+    let central: Adapter = match adapters.into_iter().nth(0) {
+        Some(c) => c,
+        None => panic!("trying get device from adapter failed")
+    };
 
     // start scan to get base stations
-    central.start_scan(ScanFilter::default()).await.unwrap();
-    time::sleep(Duration::from_secs(3)).await;
+    match central.start_scan(ScanFilter::default()).await {
+        Ok(()) => {},
+        Err(e) => panic!("trying start scanning failed {}", e)
+    }
+
+    time::sleep(Duration::from_secs(4)).await;
     
-    let base_stations = find_base_stations(&central).await.unwrap();
+    let base_stations: Vec<Peripheral> = match find_base_stations(&central).await {
+        Ok(b) => b,
+        Err(e) => panic!("No base station detected {}", e)
+    };
 
     // connect it and get service
     for p in base_stations {
-        p.connect().await.unwrap();
-        p.discover_services().await.unwrap();
+        match p.connect().await {
+            Ok(()) => {},
+            Err(e) => eprintln!("Error: trying connect base station but failed {}", e)
+        }
 
-        // if match
+        match p.discover_services().await {
+            Ok(()) => {},
+            Err(e) => eprintln!("Error: trying discover services failed {}", e)
+        }
+
         let chars = p.characteristics();
-        let cmd_char = chars.iter().find(|c| c.uuid == BASE_STATION_UUID).unwrap();
+        let cmd_char: &btleplug::api::Characteristic = match chars.iter().find(|c| c.uuid == BASE_STATION_UUID) {
+            Some(t) => t,
+            None => panic!("trying find base station uuid failed")
+        };
 
         // send command what to do
         if power_mode.to_lowercase() == "wake" {
             // wake up mode
-            p.write(&cmd_char, &[b'\x01'], WriteType::WithoutResponse).await.unwrap();
+            match p.write(&cmd_char, &[b'\x01'], WriteType::WithoutResponse).await {
+                Ok(()) => {},
+                Err(e) => eprintln!("Error: wake up command failed {}", e)
+            }
 
         } else if power_mode.to_lowercase() == "sleep" {
             // sleep mode
-            p.write(&cmd_char, &[b'\x00'], WriteType::WithoutResponse).await.unwrap();
+            match p.write(&cmd_char, &[b'\x00'], WriteType::WithoutResponse).await {
+                Ok(()) => {},
+                Err(e) => eprintln!("Error: sleep mode command failed {}", e)
+            }
+
         } else {
             panic!("power mode should name excatly \"wake\" or \"sleep\"");
         }
     }
 }
 
-async fn find_base_stations(central: &Adapter) -> Option<Vec<Peripheral>> {
+async fn find_base_stations(central: &Adapter) -> Result<Vec<Peripheral>, btleplug::Error> {
     let mut base_stations: Vec<Peripheral> = Vec::new();
 
-    for p in central.peripherals().await.unwrap() {
-        if p.properties()
-            .await
-            .unwrap()
-            .unwrap()
-            .local_name
-            .iter()
-            .any(|name| name.contains("LHB-"))
-        {
-            base_stations.push(p);
+    for p in central.peripherals().await? {
+        if let Some(name) = p.properties().await? {
+            if name.local_name.iter().any(|name| name.contains("LHB-")) {
+                println!("Found base station device: {}", name.local_name.as_deref().unwrap_or("unknown"));
+                base_stations.push(p);
+            }
         }
     }
 
-    Some(base_stations)
+    Ok(base_stations)
 }
